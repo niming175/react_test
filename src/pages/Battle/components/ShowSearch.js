bcfbf327d6634ce51f5965c56191b3be9cfa2d24
niming175 <niming175@qq.com>
2020-08-23 18:45:38 +0800

    完善battel页面

---
+++ src/pages/Battle/components/ShowSearch.js
@@ -0,0 +1,164 @@
+import React, { Component } from "react";
+import styles from "./ShowSearch.less";
+import { get } from "../../../servier/api";
+
+class ShowSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: undefined,
+      data: null,
+      loading: false,
+      error: null
+    };
+  }
+
+  componentWillUnmount() {
+    this.setState = () => {
+      return null;
+    };
+  }
+
+  submit = async e => {
+    e.preventDefault();
+    const { value, loading } = this.state;
+    const { onSearched, otherVal } = this.props;
+
+    // 处于loading状态，返回
+    if (loading) return;
+
+    // 提交的值和另外一个值相同，则报错
+    if (otherVal === value) {
+      this.setState({
+        error: "不能和另外一个值相同",
+        data: null
+      });
+      return;
+    }
+
+    this.setState({
+      loading: true,
+      error: null
+    });
+
+    // 结果， 如果有搜索到结果，将输入框的value赋值给result, 否者赋值null
+    let result = null;
+
+    try {
+      const url = `users/${value}`;
+      const response = await get(url);
+      const { data, status } = response;
+
+      if (status === 200) {
+        this.setState({
+          data
+        });
+
+        result = value;
+      } else {
+        this.setState({
+          data: null,
+          error: `请求失败，代码：${status}`
+        });
+        result = null;
+      }
+    } catch (error) {
+      const { response = {} } = error;
+      this.setState({
+        error: response.data && response.data.message
+      });
+      result = null;
+    }
+
+    this.setState({
+      loading: false
+    });
+
+    if (onSearched) onSearched(result);
+  };
+
+  // 输入框变化
+  handleChange = e => {
+    this.setState({
+      value: e.target && e.target.value
+    });
+  };
+
+  // 关闭搜索结果
+  close = () => {
+    this.setState({
+      data: null,
+      loading: false
+    });
+
+    const { onSearched } = this.props;
+    if (onSearched) onSearched(null);
+  };
+
+  render() {
+    const { title, type, placeholder } = this.props;
+    const { value, loading, data, error } = this.state;
+
+    return (
+      <div>
+        <div className={styles.from}>
+          <h3>{title}</h3>
+          {data ? (
+            <div className={styles.show}>
+              <img src={data.avatar_url} alt="" />
+              <span>{data.login}</span>
+              <a
+                role="button"
+                tabIndex="0"
+                onClick={this.close}
+                onKeyDown={this.handleKeyDown}
+              >
+                <i className="fa fa-close" />
+              </a>
+            </div>
+          ) : (
+            <form onSubmit={this.submit} style={{ marginTop: 12 }}>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <input
+                  placeholder={placeholder}
+                  style={{ flex: 1 }}
+                  value={value || ""}
+                  onChange={this.handleChange}
+                />
+                <button
+                  style={{ marginLeft: 4 }}
+                  primary="primary"
+                  type="submit"
+                  disabled={!value}
+                >
+                  {loading && (
+                    <i
+                      className="fa fa-spinner fa-spin"
+                      aria-hidden="true"
+                      style={{ marginRight: 3, fontSize: 14 }}
+                    />
+                  )}
+                  提交{type}
+                </button>
+              </div>
+            </form>
+          )}
+          {error && (
+            <p
+              style={{
+                marginTop: 10,
+                color: "red",
+                fontSize: 13,
+                textAlign: "left"
+              }}
+            >
+              {error}
+            </p>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ShowSearch;
--- src/pages/Battle/index.js
+++ src/pages/Battle/index.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { Component } from "react";
+import styles from "./index.less";
+import ShowSearch from "./components/ShowSearch";
+import { pushRoute } from "../../util/util";
 
-class Battle extends React.Component{
+class Battle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      player1: null,
+      player2: null
+    };
+  }
+
+  /**
+   * 搜索框返回
+   * @param {*} num
+   * @param {*} res
+   */
+  handleSearched = (num, res) => {
+    this.setState({
+      [`player${num}`]: res
+    });
+  };
+
+  toResult = () => {
+    const { player1, player2 } = this.state;
+    pushRoute({
+      page: "result",
+      query: {
+        player1,
+        player2
+      }
+    });
+  };
+
   render() {
+    const { player1, player2 } = this.state;
+
     return (
-      <p>Battle</p>
-    )
+      <div className={styles.main}>
+        {/* 组件 1 图片展示 */}
+        {/* 组价不一定要抽出来，单独放在components */}
+        <div className={styles.showImg}>图片展示</div>
+
+        {/* 第二部分，展示两个输入框 */}
+        <div className={styles.showWrapper}>
+          <ShowSearch
+            title="Player One"
+            placeholder="Player One"
+            onSearched={res => this.handleSearched(1, res)}
+            otherVal={player2}
+          />
+          <ShowSearch
+            title="Player Two"
+            placeholder="Player Two"
+            onSearched={res => this.handleSearched(2, res)}
+            otherVal={player1}
+          />
+        </div>
+        {/* 按钮 */}
+        <p style={{ textAlign: "center", marginTop: 30 }}>
+          <button
+            type="button"
+            primary="primary"
+            disabled={!player1 || !player2}
+            onClick={this.toResult}
+          >
+            对比
+          </button>
+        </p>
+      </div>
+    );
   }
 }
 
-export default Battle
+export default Battle;
